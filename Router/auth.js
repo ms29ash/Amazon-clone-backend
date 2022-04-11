2142aafc08237aab899ef44d4937111928976165
ms29ash <ms29ash@gmail.com>
2022-04-11 06:33:03 +0530

    login added

--- Router/auth.js
+++ Router/auth.js
@@ -10,13 +10,14 @@ const JWT_SECRET = 'authenticationvalidationcloneamazon';
 
 
 
+//Router for register user 
 authRouter.post('/register',
 
     [
         body('name', 'Enter a valid name').isLength({ min: 1 })
         ,
-        body('email').isEmail(),
-        body('password').isLength({ min: 6 })
+        body('email', 'Enter a valid email').isEmail(),
+        body('password', 'Enter a strong password').isLength({ min: 6 })
     ],
     async (req, res) => {
         const errors = validationResult(req);
@@ -60,6 +61,58 @@ authRouter.post('/register',
 
 
     })
+
+//Router for login
+authRouter.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannnot be blank').isLength({ min: 6 })
+],
+    async (req, res) => {
+        let success = false;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            // console.log(errors.array())
+            return res.status(500).json({ errors: errors.array() });
+        }
+        try {
+            let user = await User.findOne({ email: req.body.email });
+            if (!user) {
+                success = false;
+                return res.status(400).json({ errors: 'please login with valid credentials' });
+
+            }
+            const passwordCompare = await bcrypt.compare(req.body.password, user.password);
+            if (!passwordCompare) {
+                success = false;
+                return res.status(400).json({ errors: 'please login with correct credentials' });
+            }
+
+            const data = {
+                user: {
+                    id: user.id,
+                }
+            }
+
+            const authtoken = jwt.sign(data, JWT_SECRET);
+            success = true;
+
+            res.json({ success, authtoken });
+
+
+
+
+
+        } catch (error) {
+            console.log({ error })
+            res.status(500).send("internal Server Error")
+
+        }
+
+
+
+    })
+
+
 
 
 
